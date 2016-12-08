e11cd7371d9a9a26ccf7af8e64263fd234600820
NickVermeylen <nick.vermeylen@student.thomasmore.be>
2016-12-08 16:03:53 +0100

    Update

--- matchingSeq/longestSubstring.php
+++ matchingSeq/longestSubstring.php
@@ -1,8 +1,8 @@
 <?php
 
-$input1 = "ugc4qk4bbi3heev30osbvnkd8sal0jqpsoae1u89abjbgla5d83oeao06907sv993s8q87d060t4eaqbq8g242hvua0f79iahpo94qa03ur0i57v8mjk5s77ure45trugptn1khq32ns9l5vpbju3c1q43v1ihku28rjqctltojrhjltiksgqtmpdfsh4sm788se3qlbrmov7tc1uo6valh81tcoq50nkncp4t48ctd7ific0hm1k9g9rd3pnked5a";
+$input1 = "abc123abc123";
 
-$input2 = "jhebmbqkn44kgk8ng39o1vvp8c9061mr9vt9smah0o5b8fq6h9as9ai12t4j0j1c664gnp2s8cuuhh4f85gnds4eag7qibeb35n5t41r136fq4ad21l0rcspu5upnp5afot9gnllkrgrjj70iqe38j8kcrgqro9o6hgauh2tvm6gepv982uh9lskmoiofljdirf16dav3p2skk696g04hu0kiqnueuqpiu8g15rhed1rnmb8ecosqbdsbdp0apokgrhm";
+$input2 = "rtbc123a78";
 
 getLongestSubstring($input1, $input2);
 
